d368f6fb2e693904105020ff29fb8a3c0ee058a5
Osaaro <simondrohsend@gmail.com>
2023-04-27 07:48:26 +0200

    vieles neu gemacht funktioniert fast mit tailwind

--- weather_station/index.php
+++ weather_station/index.php
@@ -2,22 +2,21 @@
 $url = 'https://api.tomorrow.io/v4/timelines';
 $location = '47.1341481,8.1933634';
 $fields = 'temperature';
-$timesteps = '1h';
+$timesteps = '1m';
 $units = 'metric';
 $apikey = 'ilk1aLSD8b5CgNz9i5IOYQgkdFfvhkJf';
 
 $url .= '?location=' . urlencode($location) . '&fields=' . urlencode($fields) . '&timesteps=' . urlencode($timesteps) . '&units=' . urlencode($units) . '&apikey=' . urlencode($apikey);
 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$response = curl_exec($ch);
-curl_close($ch);
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($curl);
+curl_close($curl);
 
 $data = json_decode($response, true);
 
 $temp = $data['data']['timelines'][0]['intervals'][0]['values']['temperature'];
-
 ?>
 
 <!DOCTYPE html>
@@ -27,10 +26,10 @@ $temp = $data['data']['timelines'][0]['intervals'][0]['values']['temperature'];
             <link rel="preconnect" href="https://fonts.googleapis.com">
             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
             <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@700&display=swap" rel="stylesheet">
-            <link href="style.css">
+            <link href="style.css" rel="stylesheet">
         </head>
 
         <body>
-            <h1 class="h1">Temperatur in Sempach: <?php echo $temp; ?> &deg;C</h1>
+            <h1>Temperatur in Sempach: <?php echo $temp; ?> &deg;C</h1>
         </body>
     </html>
